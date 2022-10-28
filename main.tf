3e105b77be6dbff116837dcf08efcc3ea312f3ea
Mark Farnsworth <mark.farnsworth@iqvia.com>
2022-10-28 18:02:57 +0000

    0.0.10-s3d-1012 Changed
    - Added `s3d-flow-json` in `changes` module

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "name" {
-  source = "github.com/s3d-club/terraform-external-name?ref=0.1.10-s3d-1011"
+  source = "github.com/s3d-club/terraform-external-name?ref=0.1.10-s3d-1012"
 
   context = var.name_prefix
   keepers = { script = var.script }
